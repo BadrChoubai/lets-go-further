cb7601c1ef8f7eb0cc80c32a00fed5630ac39a36
Badr <choubai.badr@protonmail.com>
2023-06-14 23:33:26 -0600

    Setup psql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // Declare a string containing the application version number. Later in the book we'll
@@ -19,24 +23,50 @@ const version = "1.0.0"
 // server to listen on, and the name of the current operating environment for the
 // application (development, staging, production, etc.). We will read in these
 // configuration settings from command-line flags when the application starts.
-type config struct {
-	port int
-	env  string
-}
+type (
+	connectionPoolSettings struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 
-type application struct {
-	appConfig config
-	log       *log.Logger
-}
+	config struct {
+		port int
+		env  string
+		db   connectionPoolSettings
+	}
+
+	application struct {
+		appConfig config
+		log       *log.Logger
+	}
+)
 
 func main() {
 	var config config
 
 	flag.IntVar(&config.port, "port", 4000, "API server port")
-	flag.StringVar(&config.env, "env", "development", "API server environment")
+	flag.StringVar(&config.env, "env", "development", "Environment (development|staging|production)")
+
+	// Setup DB Connection Pool
+	flag.StringVar(&config.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&config.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&config.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&config.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDb(config)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("PSQL: connection pool established")
 
 	application := &application{
 		appConfig: config,
@@ -53,6 +83,33 @@ func main() {
 
 	// Start the HTTP server.
 	logger.Printf("starting %s server on http://127.0.0.1%s", config.env, server.Addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDb(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
