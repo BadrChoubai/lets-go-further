33d65df788e59f61fd27ecd1acaf2c91946fea8d
Badr <choubai.badr@protonmail.com>
2023-06-16 16:14:56 -0600

    Make changes

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
+PRIMARY_DB = ${GREENLIGHT_DB_DSN}
+
 .PHONY: run
 run:
 	go run ./cmd/api
+
+.PHONY: migrate_up
+migrate_up: migrate_down
+	migrate -path ./migrations -database ${PRIMARY_DB} up
+
+.PHONY: migrate_down
+migrate_down:
+	migrate -path ./migrations -database ${PRIMARY_DB} down
--- migrations/000002_add_movies_check_constraints.down.sql
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -2,7 +2,9 @@ ALTER TABLE movies DROP CONSTRAINT IF EXISTS movies_runtime_check;
 
 ALTER TABLE movies DROP CONSTRAINT IF EXISTS movies_year_check;
 
-ALTER TABLE movies DROP CONSTRAINT IF EXISTS genres_length_check;ALTER TABLE movies
+ALTER TABLE movies DROP CONSTRAINT IF EXISTS genres_length_check;
+
+ALTER TABLE movies
   DROP CONSTRAINT IF EXISTS movies_runtime_check;
 ALTER TABLE movies
   DROP CONSTRAINT IF EXISTS movies_year_check;
