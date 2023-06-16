035e4ff704cca2828d586eeeb6c873a146a8031c
Badr <choubai.badr@protonmail.com>
2023-06-16 16:29:11 -0600

    Add sorting properties to sql query

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "greenlight.badrchoubai.dev/internal/validator"
+import (
+	"greenlight.badrchoubai.dev/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page           int
@@ -16,4 +19,21 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortableValues...), "sort", "invalid sort values")
+}
+
+func (f Filters) sortColumn() string {
+	for _, sortableValue := range f.SortableValues {
+		if f.Sort == sortableValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
