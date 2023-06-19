a445ce75a48ed5bd9de10ef91decdb18fb3a264e
Badr <choubai.badr@protonmail.com>
2023-06-19 11:41:32 -0600

    Add data model for authorization tokens/scope

--- internal/data/models.go
+++ internal/data/models.go
@@ -2,26 +2,20 @@ package data
 
 import "database/sql"
 
-type UserModel struct {
-	DB *sql.DB
-}
-
-type MovieModel struct {
-	DB *sql.DB
-}
+type UserModel struct{ DB *sql.DB }
+type MovieModel struct{ DB *sql.DB }
+type TokenModel struct{ DB *sql.DB }
 
 type Models struct {
 	Movies MovieModel
 	Users  UserModel
+	Token  TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{
-			DB: db,
-		},
-		Users: UserModel{
-			DB: db,
-		},
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
+		Token:  TokenModel{DB: db},
 	}
 }
