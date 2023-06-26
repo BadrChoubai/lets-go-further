44bb1d1dd8763f631949b978fc1db50045c3a737
Badr <choubai.badr@protonmail.com>
2023-06-26 12:55:58 -0600

    Add permissions model

--- internal/data/models.go
+++ internal/data/models.go
@@ -2,14 +2,16 @@ package data
 
 import "database/sql"
 
-type UserModel struct{ DB *sql.DB }
 type MovieModel struct{ DB *sql.DB }
+type PermissionModel struct{ DB *sql.DB }
 type TokenModel struct{ DB *sql.DB }
+type UserModel struct{ DB *sql.DB }
 
 type Models struct {
-	Movies MovieModel
-	Users  UserModel
-	Token  TokenModel
+	Movies      MovieModel
+	Permissions PermissionModel
+	Token       TokenModel
+	Users       UserModel
 }
 
 func NewModels(db *sql.DB) Models {
